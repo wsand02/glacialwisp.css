6cd58795226fba672be6a142c65df5b0f82bafbd
William Sandbrink <29465936+wsand02@users.noreply.github.com>
2023-06-10 20:48:18 +0200

    update deps

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,6 @@
-const gulp = require("gulp");
-const { parallel } = require("gulp");
-const sass = require('gulp-sass');
+const gulp = require('gulp');
+const { parallel } = require('gulp');
+const sass = require('gulp-sass')(require('sass'));
 const cleancss = require('gulp-clean-css');
 const csscomb = require('gulp-csscomb');
 const rename = require('gulp-rename');
@@ -10,41 +10,47 @@ const autoprefixer = require('gulp-autoprefixer');
 function build() {
   return gulp
     .src('./src/*.scss')
-    .pipe(sass({outputStyle: 'compact', precision: 10})
-      .on('error', sass.logError)
+    .pipe(
+      sass({ outputStyle: 'compact', precision: 10 }).on('error', sass.logError)
     )
     .pipe(autoprefixer())
     .pipe(csscomb())
     .pipe(gulp.dest('./dist'))
     .pipe(cleancss())
-    .pipe(rename({
-      suffix: '.min'
-    }))
+    .pipe(
+      rename({
+        suffix: '.min',
+      })
+    )
     .pipe(gulp.dest('./dist'));
 }
 
 function docs_css() {
   return gulp
     .src(['./src/*.scss', './docs/src/scss/*.scss'])
-    .pipe(sass({outputStyle: 'compact', precision: 10})
-      .on('error', sass.logError)
+    .pipe(
+      sass({ outputStyle: 'compact', precision: 10 }).on('error', sass.logError)
     )
     .pipe(autoprefixer())
     .pipe(csscomb())
     .pipe(gulp.dest('./docs/dist'))
     .pipe(cleancss())
-    .pipe(rename({
-      suffix: '.min'
-    }))
+    .pipe(
+      rename({
+        suffix: '.min',
+      })
+    )
     .pipe(gulp.dest('./docs/dist'));
 }
 
 function docs_pug() {
   return gulp
     .src('docs/src/**/!(_)*.pug')
-    .pipe(pug({
-      pretty: true
-    }))
+    .pipe(
+      pug({
+        pretty: true,
+      })
+    )
     .pipe(gulp.dest('./docs/'));
 }
 
